73929cd526f57f1cf05a493a0ab960fb53613d17
hsikander <hammad.sikander2@gmail.com>
2021-02-27 16:41:28 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function sum (a , b) {
+   
+//  return 
+// }
+// console.log(sum(5 , 5))
+
+var value1 = +prompt("Enter First value");
+var value2 = +prompt("Enter Second Value");
+
+var result = value1 + value2;
